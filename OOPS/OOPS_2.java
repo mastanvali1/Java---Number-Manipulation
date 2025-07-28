6b0487774596487cc751e13a6b47105a6ea6fd36
mastanvali1 <mastanshaik1331@gmail.com>
2025-07-28 21:00:16 +0530

    OOPS

---
+++ OOPS/OOPS_2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+//class : - class is the definition of object that which contains properties and behaviours.
+
+class student{
+    int roll;
+    String name;
+    String destination;
+    double salary;
+    void work() {
+        System.out.println("working");
+    }
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+
+ public class OOPS_2 {
+    public static void main(String[] args) {
+        // object : -  object is the programatical representation of class or instace of class.
+
+        student st = new student();
+        Scanner scan = new Scanner(System.in);
+        st.roll = scan.nextInt();
+        st.name = scan.next();
+        st.destination = scan.next();
+        st.work();
+        st.eat();
+
+        new student().work();  //anonymouse object.
+    }
+}
+
+/*
+Basics of class and object creation with defination.
+
+anonymouse object:-  object are such objects which are not having any reference variable and we can use them only once
+
+*/

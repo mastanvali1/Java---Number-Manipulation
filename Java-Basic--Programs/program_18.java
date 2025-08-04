c916ac72fa3ad818142878b09ee341cd8b6a17a1
mastanvali1 <mastanshaik1331@gmail.com>
2025-08-04 21:51:52 +0530

    Added some more basic programs

--- Java-Basic--Programs/program_18.java
+++ Java-Basic--Programs/program_18.java
@@ -1,10 +1,17 @@
-    /*---------program for Temperature predictor---------- */
+    /*---------Count number of digits in a given number---------- */
 
 import java.util.*;
-public class program_17 {
+public class program_18 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int temp = scan.nextInt();
-        
+		int num = scan.nextInt();
+		int a = num;
+		int count = 0;
+		while (num > 0) {
+			num = num / 10;
+			count++;
+		}
+		System.out.println("The number of digits in give "+ a +" is " + count);
+
 	}	
 }

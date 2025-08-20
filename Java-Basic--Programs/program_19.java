abe4c18538cbb445dfd013e85f5ff2b5a0384a72
mastanvali1 <mastanshaik1331@gmail.com>
2025-08-20 08:34:41 +0530

    modified the  program

--- Java-Basic--Programs/program_19.java
+++ Java-Basic--Programs/program_19.java
@@ -1,19 +1,21 @@
+
+//--------- Convert decimal to binary-----
+
 import java.util.Scanner;
 public class program_19 {
     public static void main(String[] args) {
         // Use Scanner to take user input
         Scanner scanner = new Scanner(System.in);
-        int decimal = scanner.nextInt();
-        int n = decimal;
-
-        // Convert decimal to binary
+        int num = scanner.nextInt();
         String res = "";
-        while(decimal != 0){
-            int rem = decimal%2;
+        
+        while(num >= 1) {
+            int rem = num % 2;
             res = rem + res;
-            decimal= decimal/2;
-       }
-        System.out.println("The binary equivalent of " + n + " is " + res + ".");
-}
+            num = num / 2;
+        }
+        System.out.println("the binary of given number is :" +  res);
+   
+    }
 
 }
